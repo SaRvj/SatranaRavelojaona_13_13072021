a3e54ee1ecf8a27b8a259c6b2ad4178da4835632
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-05-27 17:11:47 +0200

    add mocked data for features

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+import iconChat from "../assets/Images/icon-chat.png";
+import iconMoney from "../assets/Images/icon-money.png";
+import iconSecurity from "../assets/Images/icon-security.png";
+
+export const featureData = [
+    {
+      imgSrc: iconChat,
+      alt: "Chat Icon",
+      title: "You are our #1 priority",
+      text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+    },
+    {
+      imgSrc: iconMoney,
+      alt: "Money Icon",
+      title: "More savings means higher rates",
+      text: "The more you save with us, the higher your interest rate will be!",
+    },
+    {
+      imgSrc: iconSecurity,
+      alt: "Security Icon",
+      title: "Security you can trust",
+      text: "We use top of the line encryption to make sure your data and money is always safe.",
+    },
+];
