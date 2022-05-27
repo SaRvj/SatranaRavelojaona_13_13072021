f996929d45332f3332abc31c20058d3fdff72ae6
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-05-27 19:04:27 +0200

    add return to sign in if user not connected

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,42 @@
+import { transactionData } from "../data/data"
+import TransactionCard from "../components/TransactionInfos"
+import { useEffect } from "react"
+import { selectJWT, selectUserLogin } from "../utils/Selectors"
+import { useDispatch, useSelector } from "react-redux"
+import ApiCalls from "../utils/CallMethod"
+import { setUserData } from "../redux/Reducers"
+import { Navigate } from "react-router-dom"
+
+function Profile() {
+  let dispatch = useDispatch()
+  let JWT = useSelector(selectJWT)
+  const connected = useSelector(selectUserLogin)
+
+  useEffect(() => {
+      async function getUserProfile() {
+          const response = await new ApiCalls().getUserProfileData(JWT)
+          console.log(response.data.body)
+          dispatch(setUserData(response.data.body))
+          console.log(setUserData);
+          return response
+      }
+      getUserProfile() 
+  }, [JWT, dispatch])
+  
+  if(connected === false || connected === undefined || connected === null) {
+    return <Navigate to='/sign-in'/>
+  }
+  
+    return (
+        <main className="main bg-dark">
+            <h2 className="sr-only">Accounts</h2>
+              {transactionData.map(({ id, title, amount, description }) => {
+                return (
+                  <TransactionCard key={id} title={title} amount={amount} description={description}/>
+                );
+              })}
+        </main>
+    )
+}
+
+export default Profile
