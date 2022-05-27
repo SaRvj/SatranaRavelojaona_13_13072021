56320bc17e18fad1d1bf712b4d9b4cdf5f01a396
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-05-27 17:43:39 +0200

    add login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import LoginForm from "../components/LoginForm";
+import React from "react";
+
+function Login() {
+    return (
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+                <LoginForm />
+            </section>
+        </main>
+    )
+}
+
+export default Login
