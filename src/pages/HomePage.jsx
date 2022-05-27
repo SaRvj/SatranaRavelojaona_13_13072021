71dfc17c7ba666b5708b3ab8fa561297e21d604f
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-05-27 17:18:10 +0200

    add Homepage and featureitem

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import Feature from '../components/FeatureItem'
+import { featuresData } from '../data/data'
+
+function HomePage() {
+    return (
+        <main className="homepage">
+            <div className="hero">
+                <section className="hero-content">
+                <h2 className="sr-only">Promoted Content</h2>
+                <p className="subtitle">No fees.</p>
+                <p className="subtitle">No minimum deposit.</p>
+                <p className="subtitle">High interest rates.</p>
+                <p className="text">Open a savings account with Argent Bank today!</p>
+                </section>
+            </div>
+            <section className='features'>
+                <h2 className='sr-only'>Features</h2>
+                {featuresData && featuresData.map((e, i) => (
+                <Feature key={i} src={e.imgSrc} alt={e.alt} title={e.title} text={e.text} />
+                ))}
+            </section>
+        </main>
+    )
+}
+
+export default HomePage
