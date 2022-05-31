7f4ac6677c5480f0aa69e2a32cb7a710a836589d
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-05-31 13:53:25 +0200

    returned as before

--- src/utils/CallMethod.jsx
+++ src/utils/CallMethod.jsx
@@ -2,8 +2,7 @@ import axios from "axios"
 
 class ApiCalls {
     async userLogIn(email, password, rememberMe) {
-        console.log(email, password);
-        return axios.post('http://localhost:3001/api/v1/user/login', {
+        return await axios.post('http://localhost:3001/api/v1/user/login', {
             "email": email,
             "password": password
         })
@@ -22,8 +21,8 @@ class ApiCalls {
             }
         });
     }
-
-    async getUserProfileData(JWT) {
+    
+      async getUserProfileData(JWT) {
         return await axios.post("http://localhost:3001/api/v1/user/profile", {}, {
             headers: {
                 Authorization: "Bearer " + JWT
@@ -35,25 +34,7 @@ class ApiCalls {
         .catch (function (error) {
             return error
         }) 
-    }
-
-    async updateUserProfileData(firstName, lastName, JWT) {
-        return await axios.put("http://localhost:3001/api/v1/user/profile", {
-            firstName,
-            lastName
-        },
-        {
-            headers: {
-                Authorization: "Bearer " + JWT
-            }
-        })
-        .then(function (response) {
-            return response
-        })
-        .catch(function (error) {
-            return error
-        })
-    }   
+      }
 }
   
 export default ApiCalls;
