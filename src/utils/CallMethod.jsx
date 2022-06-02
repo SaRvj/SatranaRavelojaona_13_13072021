6b7b086350df9fa1a60a15237abc3390ba2b436d
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-06-02 12:47:48 +0200

    add modify name

--- src/utils/CallMethod.jsx
+++ src/utils/CallMethod.jsx
@@ -22,7 +22,7 @@ class ApiCalls {
         });
     }
     
-      async getUserProfileData(JWT) {
+    async getUserProfileData(JWT) {
         return await axios.post("http://localhost:3001/api/v1/user/profile", {}, {
             headers: {
                 Authorization: "Bearer " + JWT
@@ -34,7 +34,25 @@ class ApiCalls {
         .catch (function (error) {
             return error
         }) 
-      }
+    }
+
+    async updateUserProfileData(firstName, lastName, JWT) {
+        return await axios.put("http://localhost:3001/api/v1/user/profile", {
+          firstName,
+          lastName
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + JWT
+          }
+        })
+        .then(function (response) {
+          return response
+        })
+        .catch(function (error) {
+          return error
+        })
+    }   
 }
   
 export default ApiCalls;
